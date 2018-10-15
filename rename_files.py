7753732657e2650c7783943eea4b24a79338434a
tfcosta <fkuste@gmail.com>
2018-10-15 07:30:47 -0300

    Create function rename_files and add os.listdir

---
+++ rename_files.py
@@ -0,0 +1,9 @@
+import os
+def rename_files ():
+    #(1) Obter os nomes dos arquivos em uma pasta
+    file_list = os.listdir(r"C:\Users\Avell 1513\Desktop\python\rename_files\prank")
+    print(file_list)
+
+    #(2) Renomear cada arquivo
+
+rename_files()
